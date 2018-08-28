e0b16eb2097707a0398f9b4d65415847cb057796
Tarun Yadav <tarunyadav@Taruns-MBP.attlocal.net>
2018-08-28 01:27:52 -0700

    setting up the inital files

---
+++ src/prediction-validation.py
@@ -0,0 +1,5 @@
+# Date Created - 08/28/2019
+# Author -Tarun Yadav
+# This files contains the code for calculating the average error between predicted and actual of matching stocks within a time window 
+
+
