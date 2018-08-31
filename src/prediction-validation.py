34e091236c55aed675d941a7de8426840f8fcf3e
Tarun Yadav <tarunyadav@Taruns-MBP.attlocal.net>
2018-08-31 05:39:08 -0700

    Get the counts and sums for time windows

--- src/prediction-validation.py
+++ src/prediction-validation.py
@@ -2,32 +2,52 @@
 # Author -Tarun Yadav
 # This files contains the code for calculating the average error between predicted and actual of matching stocks within a time window 
 
+#!/usr/bin/python
+
 import csv
+import operator
 
-# Read the input files
-def read_input():
+def error(actualList,predictedList):
+  errors=[]
+  for x1 in predictedList:
+    for x2 in actualList:
+      if (x1[0]==x2[0] and x1[1]==x2[1]):
+        errors.append((x1[0],round(abs(float(x1[2])-float(x2[2])),2)))
+  return errors
 
-    actualFile=open("./input/actual.txt","r")
-    actualReader=csv.reader(actualFile,delimiter="|")
-    actualList=[tuple(l) for l in list(actualReader)]
-    actualFile.close()
-    print(actualList)
+def test():
+  actualFile=open("actual.txt","r")
+  actualReader=csv.reader(actualFile,delimiter="|")
+  actualList=[tuple(l) for l in list(actualReader)]
+  actualFile.close()
 
-    predictedFile=open("./input/predicted.txt","r")
-    predictedReader=csv.reader(predictedFile,delimiter="|")
-    predictedList=[tuple(l) for l in list(predictedReader)]
-    predictedFile.close()
-    print(predictedList)
 
-    windowFile=open("./input/window.txt","r")
-    window=windowFile.read()
-    windowFile.close()
-    print(window)
+  predictedFile=open("predicted.txt","r")
+  predictedReader=csv.reader(predictedFile,delimiter="|")
+  predictedList=[tuple(l) for l in list(predictedReader)]
+  predictedFile.close()
 
-read_input()
+  windowFile=open("window.txt","r")
+  window=windowFile.read()
+  windowFile.close()
+  
+  maxTime=predictedList[len(predictedList)-1][0]
+  minTime=predictedList[0][0]
 
-# Match the stocks between actual and predicted 
+  numLoops=(int(maxTime)-int(window))+1
+  
+  start=int(minTime)
 
-# Get the average error for the time window
+  errors=error(actualList,predictedList)
 
-# Write to the output file
+  my_set = {x[0] for x in errors}
+  my_list=[x[0] for x in errors]
+
+  my_sums = sorted([(i,sum(x[1] for x in errors if x[0] == i),my_list.count(i)) for i in my_set])
+
+
+  while start<=numLoops:
+    
+    start += 1
+
+test()
