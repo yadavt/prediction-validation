6f71a46fe61a42d84f114ee922714d074784ce35
Tarun Yadav <tarunyadav@Taruns-MBP.attlocal.net>
2018-08-31 08:49:45 -0700

    Final changes

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
 #python ./src/prediction-validation.py ./input/window.txt ./input/actual.txt ./input/predicted.txt ./output/comparison.txt
+
+# provide your input files in the ./input folder and run the command below to kick off the job
 python3 ./src/prediction-validation.py
